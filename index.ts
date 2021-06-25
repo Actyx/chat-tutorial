56120f37cf3394fa1e22ba4ee63db2d541a74448
nicolas-actyx <nicolas@actyx.io>
2021-06-25 15:04:37 +0200

    updating to Cosmos/0761f5016

--- index.ts
+++ index.ts
@@ -1,47 +1,60 @@
-import { FishId, Pond, Fish, Tag } from '@actyx/pond'
+// [[start:import]]
+import { FishId, Pond, Fish, Tag } from '@actyx/pond';
+import manifest from './manifest.json';
+// [[end:import]]
 
-type ChatEvent = string
-type ChatState = ChatEvent[]
+// [[start:event-and-state]]
+type ChatEvent = string;
+type ChatState = ChatEvent[];
+// [[end:event-and-state]]
 
-const INITIAL_STATE: ChatState = []
+// [[start:initial-state]]
+const INITIAL_STATE: ChatState = [];
+// [[end:initial-state]]
 
+// [[start:on-event]]
 function onEvent(state: ChatState, event: ChatEvent) {
-  state.push(event)
-  return state
+  state.push(event);
+  return state;
 }
+// [[end:on-event]]
 
-const chatTag = Tag<ChatEvent>('ChatMessage')
+// [[start:fish]]
+const chatTag = Tag<ChatEvent>('ChatMessage');
 const ChatFish: Fish<ChatState, ChatEvent> = {
   fishId: FishId.of('ax.example.chat', 'MyChatFish', 0),
   initialState: INITIAL_STATE,
   onEvent: onEvent,
   where: chatTag,
-}
+};
+// [[end:fish]]
 
-Pond.default()
+// [[start:pond]]
+Pond.default(manifest)
   .then((pond) => {
     // Select UI elements in the DOM
-    const messagesTextArea = document.getElementById('messages')
-    const messageInput = <HTMLInputElement>document.getElementById('message')
-    const sendButton = document.getElementById('send')
+    const messagesTextArea = document.getElementById('messages');
+    const messageInput = <HTMLInputElement>document.getElementById('message');
+    const sendButton = document.getElementById('send');
 
     function clearInputAndSendToStream() {
       // When click on send button get the text written in the input field
-      const message = messageInput.value
-      messageInput.value = ''
+      const message = messageInput.value;
+      messageInput.value = '';
       // Send the message to a stream tagged with our chat tag
-      pond.emit(chatTag, message)
+      pond.emit(chatTag, message);
     }
 
-    sendButton.addEventListener('click', clearInputAndSendToStream)
+    sendButton.addEventListener('click', clearInputAndSendToStream);
 
     // Observe our chat fish. This means that our callback function will
     // be called anytime the state of the fish changes
     pond.observe(ChatFish, (state) => {
       // Get the `pre` element and add all chat messages to that element
-      messagesTextArea.innerHTML = state.join('\n')
+      messagesTextArea.innerHTML = state.join('\n');
       // Scroll the element to the bottom when it is updated
-      messagesTextArea.scrollTop = messagesTextArea.scrollHeight
-    })
+      messagesTextArea.scrollTop = messagesTextArea.scrollHeight;
+    });
   })
-  .catch(console.log)
+  .catch(console.log);
+// [[end:pond]]
